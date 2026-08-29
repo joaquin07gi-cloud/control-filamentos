4a8a80c2abbd5f5d3775bc015c5a5f30c0d52138
joaquin07gi-cloud <joaquin07gi@gmail.com>
2026-08-29 19:02:03 -0300

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Control de Filamentos 3D", page_icon="🧵", layo
 # Conexión solo para LEER los datos actualizados
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxrpvcw9yX10yZYF-kcRcVXs9rWk9MIHlsTd2BUV4OMcudgK0TjPFQmrGiwdiHJAXenpw/exec"
+URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxrpvcw9yX10yZYF-kcRcVXs9rWk9MIHlsTd2BUV40McudgK0TjPFQmrGiwdiHJAXenpw/exec"
 
 st.title("🧵 Control de Filamentos 3D")
 st.caption("Inventario sincronizado en tiempo real")
